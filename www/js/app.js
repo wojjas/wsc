5ae63a9438608282e80e3a7adf7ec0a0c19771d6
Wojciech Jasinski <wojtek@proxoft.se>
2016-06-15 00:29:36 +0200

    Removed the Account tab

--- www/js/app.js
+++ www/js/app.js
@@ -67,16 +67,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             controller: 'ChatDetailCtrl'
           }
         }
-      })
-
-      .state('tab.account', {
-        url: '/account',
-        views: {
-          'tab-account': {
-            templateUrl: 'templates/tab-account.html',
-            controller: 'AccountCtrl'
-          }
-        }
       });
 
     // if none of the above states are matched, use this as the fallback
