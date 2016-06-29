23d371825d393399055cdbd0ed811cb10d946935
Wojciech Jasinski <wojtek@proxoft.se>
2016-06-29 13:47:19 +0200

    Removed unised select-all-on-focus/click

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -266,20 +266,6 @@ angular.module('starter.controllers', [])
     }
   })
 
-  .directive('selectOnClick', ['$window', function ($window) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        element.on('click', function () {
-          if (!$window.getSelection().toString()) {
-            // Required for mobile Safari
-            this.setSelectionRange(0, this.value.length)
-          }
-        });
-      }
-    };
-  }])
-
   .directive('clearingInput', [function () {
     // Usage:
     //  <clearing-input value="ClearingInputTestCtrl.someValue"></clearing-input>
